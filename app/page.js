3f7522d0a264e7f2d273a37c084f21748dc7f0f9
Ngawang Tsetan <ngatsetan2@gmail.com>
2024-07-20 18:28:56 -0400

    update problem section and withwout section

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Suspense } from 'react'
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Problem from "@/components/Problem";
-import FeaturesAccordion from "@/components/FeaturesAccordion";
+import WithWithout from "@/components/WithWithout";
 import Pricing from "@/components/Pricing";
 import FAQ from "@/components/FAQ";
 import CTA from "@/components/CTA";
@@ -18,7 +18,7 @@ export default function Home() {
       <main>
         <Hero />
         <Problem />
-        <FeaturesListicle />
+        <WithWithout />
         <Pricing />
         <FAQ />
         <CTA />
