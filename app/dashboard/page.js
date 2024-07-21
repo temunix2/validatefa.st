f28b21de4c5e2e4bf7e3437cbf1db07d290391dd
Ngawang Tsetan <ngatsetan2@gmail.com>
2024-07-21 13:32:44 -0400

    add gradient button to dashboard, add opengraph and twitter image

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,24 +1,45 @@
+import Header from "@/components/Header";
 import ButtonAccount from "@/components/ButtonAccount";
 import ButtonCheckout from "@/components/ButtonCheckout";
+import ButtonGradient from "@/components/ButtonGradient";
 import config from "@/config";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/libs/next-auth";
+import connectMongo from "@/libs/mongoose";
+import User from "@/models/User";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
 export default async function Dashboard() {
+  await connectMongo();
+  const session = await getServerSession(authOptions);
+  const user = await User.findById(session.user.id);
+
+
   return (
+    <>
+    <Header />
+
     <main className="min-h-screen p-8 pb-24">
       <section className="max-w-xl mx-auto space-y-8">
+        
+        
         <ButtonAccount />
-
         <h1 className="text-3xl md:text-4xl font-extrabold">
-          Subscribe to get access:
-        </h1>
+            User Dashboard
+          </h1>
+          <p>Welcome {user.name} 👋</p>
+          <p>Your email is {user.email}</p>
 
-        <ButtonCheckout
-          mode="subscription"
-          priceId={config.stripe.plans[0].priceId}
-        />
+          <div className="my-8">
+            <Link href="/persona-chat" passHref>
+              <ButtonGradient title="Go to Persona Chat" />
+            </Link>
+          </div>
+
       </section>
     </main>
+    </>
   );
 }
--- app/persona-chat/page.js
+++ app/persona-chat/page.js
@@ -121,7 +121,7 @@ Whether you're crafting the next bestseller, penning poetry, or polishing your p
     <LayoutClient>
       <Header />
       <div className="max-w-6xl mx-auto p-4 bg-base-100">
-        <h1 className="text-3xl font-bold text-primary mb-4">Chat with Writing {personaTerms.plural}</h1>
+        <h1 className="text-3xl font-bold text-primary mb-4">{personaTerms.plural} Chat </h1>
         <div className="flex">
           {/* Chat Section */}
           <div className="flex-grow mr-4">
